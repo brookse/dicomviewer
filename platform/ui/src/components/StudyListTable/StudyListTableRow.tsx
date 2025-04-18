d2a169bb46091b5becdde7c600200f437fc2997e
Lyzzi Brooks <lyzziramonee@gmail.com>
2025-04-18 11:10:39 -0500

    hisfhudks

--- platform/ui/src/components/StudyListTable/StudyListTableRow.tsx
+++ platform/ui/src/components/StudyListTable/StudyListTableRow.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import getGridWidthClass from '../../utils/getGridWidthClass';
 import { Icons } from '@ohif/ui-next';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import getGridWidthClass from '../../utils/getGridWidthClass';
 
 const StudyListTableRow = props => {
   const { tableData } = props;
